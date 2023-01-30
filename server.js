b6af6e9d4e88902ceaa6c2f9c6f490171e708661
Marco Pontiz <marcopontiz@gmail.com>
2023-01-30 05:56:56 -0300

    Arreglando el desafío

--- server.js
+++ server.js
@@ -2,33 +2,30 @@ import express from `express`;
 import cluster from `cluster`;
 import * as os from `os`;
 
-const modoCluster = process.argv[3] || `CLUSTER`;
+const modoCluster = process.argv[3] == `CLUSTER`;
 
-//Master
-if(modoCluster && cluster.isPrimary) {
-    const numCPUs = os.cpus(). length;
-
+if (modoCluster && cluster.isPrimary) {
+    const numCPUs = os.cpus().length;
     console.log(`Primary ${process.pid} is running`);
-    console.log(`Numero de procesadores ${numCPUs}`);
+    console.log(`número de procesadores: ${numCPUs}`);
 
-    for(let i = 0; i < numCPUs; i++){
+for(let i = 0; i < numCPUs; i++) {
     cluster.fork();
-    }
+}
 
-    cluster.on(`exit`, worker => {
+cluster.on(`exit`, worker => {
     console.log(`worker ${worker.process.pid} died`, new Date().toLocaleString());
     cluster.fork();
-    })
+})
 }
 else {
     const app = express();
     const PORT = parseInt(process.argv[2]) || 8080;
+    const STATIC = process.argv[4] == `STATIC`;
 
-    app.get(`/datos`, (req, res) => {
-        res.send(`Server en Port(${PORT}) - PID ${process.pid} - ${new Date().toLocaleString()}`);
-    })
-    const server = app.listen(PORT, () => {
-        console.log(`Servidor express escuchando en http://localhost:${PORT}`);
-    });
-    server.on(`error`, error => console.log(`Error del servidor ${error}`));
-}
+if(STATIC){
+
+    app.use(express.static(`/public`));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+}}
